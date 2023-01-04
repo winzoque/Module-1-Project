d8f691d8b1557b6ce502c9ad6863d177a52b773f
winzoque <winzoque@gmail.com>
2023-01-05 07:43:26 +0800

    Update main.test.js

--- main.test.js
+++ main.test.js
@@ -2,5 +2,17 @@ import { sort, search, newsList } from "./main.js";
 
 //Example testing for search function
 test("Search function testing", () => {
-  expect(search(newsList)).toBe("I am a cool web developer");
+  expect(search("web")).toBe("I am a cool web developer");
+});
+
+test("Search function testing", () => {
+  expect(search("what films")).toBe("What films should an aspiring journalist watch?");
+});
+
+test("Search function testing", () => {
+  expect(search("tv")).toBe("'Nightmare' TV show 'Euphoria — health threat or high art?");
+});
+
+test("Search function testing", () => {
+  expect(search("Colvin")).toBe("Marie Colvin shined a light on war-torn corners of the world");
 });
